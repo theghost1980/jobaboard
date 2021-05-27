8afa5a6c576d46796b0a3951a3ce41e681f10dd7
Saturno Mangieri <saturnob612@gmail.com>
2021-05-27 08:21:57 -0400

    adding google tag manager, testing live now

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-R2Q5MM37J0", // Google Analytics / GA G-R2Q5MM37J0
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
